57cb8d12586fd7f885fb94a48472d34551a8bde9
BAGAS SEBASTIAN <111202214715@mhs.dinus.ac.id>
2026-04-30 20:00:04 +0700

    format emotion_detector output

--- emotion_detection.py
+++ emotion_detection.py
@@ -4,7 +4,22 @@ import json
 URL='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 HEADER={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
+def __dominant_emotion_format(response:dict):
+   response=response['emotionPredictions']['emotion']
+   del response['target']
+   del response['emotionMentions']
+   max_key='';max_value=0.0
+   for k,v in response.items():
+      if v > max_value:
+         max_key=k 
+         max_value=v
+   response['dominant_emotion']=max_key
+   return response
+        
 def emotion_detector(text_to_analyze):
-    payload={ "raw_document": { "text": text_to_analyze } }
-    response=requests.post(url,json=payload,headers=HEADER)
-    return json.loads(response.text)
+   payload={ "raw_document": { "text": text_to_analyze } }
+   response=requests.post(URL,json=payload,headers=HEADER)
+   response=json.loads(response.text)
+   return __dominant_emotion_format(response)
+
+EXAMPLE='Pentingnya PENDIDIKAN! Daripada jadi DUNGU macam supir² yg menghadang'
