e5b2c856a42f37361df16fc9bb6fd247c1c9fa4a
BAGAS SEBASTIAN <111202214715@mhs.dinus.ac.id>
2026-05-01 08:01:44 +0700

    create homepage controller

--- server.py
+++ server.py
@@ -1,23 +1,31 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app=Flask(__name__)
 
-@app.route('/')
-def detect_emotion():
-    input=request.args.get('input')
-    output=emotion_detector(input)
-    if output is None:
-        return ('input argument required', 400)
+def __format_output(payload):
     response_txt="For the given statement, the system response is "
-    dominant=output['dominant_emotion']
-    del output['dominant_emotion']
-    for k,v in output.items():
-        response_txt+=f"\'{k}\':{v}, "
+    dominant=payload['dominant_emotion']
+    del payload['dominant_emotion']
+    for k,v in payload.items():
+        response_txt+=f"\'{k}\': {v}, "
     response_txt = response_txt.removesuffix(" ")+'.'
     response_txt+=f"The dominant emotion is <b>{dominant}</b>"
-    # 'anger': 0.006274985, 'disgust': 0.0025598293, 'fear': 0.009251528, 'joy': 0.9680386 and 'sadness': 0.049744144. The dominant emotion is joy."
     return response_txt
+
+@app.route('/')
+def homepage():
+    """This controller return index.html"""
+    return render_template('index.html')
+
+@app.route('/emotionDetector')
+def detect_emotion():
+    input=request.args.get('textToAnalyze')
+    output=emotion_detector(input)
+    if output is None:
+        return ""
+    return __format_output(output)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
